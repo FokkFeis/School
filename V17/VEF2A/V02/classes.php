befd1bd6b57202188c21bc17a0dd0f00366feee7
Sigurður Baldvin Friðriksson <sigurdurbaldvi@gmail.com>
2017-01-28 00:33:08 +0000

    bunch-a-fucken php

--- V17/VEF2A/V02/classes.php
+++ V17/VEF2A/V02/classes.php
@@ -1,6 +1,7 @@
 <?php
 
-class Image {
+class image {
+
     public function __construct($name, $size, $owner, $path)
     {
       $this->name = $name;
@@ -10,37 +11,99 @@ class Image {
     }
 
     public function getImageName() {
-
+      return $this->name;
     }
     public function getOwnerName() {
-
+      return $this->owner;
     }
     public function getImageSize() {
-
+      $this->size = filesize($this->path);
+      $this->size =$this->size /1024;
+      return $this;
     }
     public function getPath() {
-
+      return $this->Path = $path;
     }
     public function setImageName() {
-
+      $this->ImageName = $NewName;
     }
     public function setPath() {
-
+      $this->Path = $NewPath;
     }
     public function setOwnerName() {
-
+      $this->OwnerName = $newOwnerName;
     }
     public function showImageSize() {
-
+      echo '<p>Image is ' . $this->size . ' kb';
     }
     public function showImage() {
-
+      echo '<img src="' . $this->path .'"/>'."\n";
     }
-    ]
 }
 
-class Book
-{
+class upload {
+
+
+    protected $uploaded = [];
+    protected $destination;
+    protected $max = 51200;
+    protected $messages = [];
+    protected $permitted = [
+        'image/gif',
+        'image/jpeg',
+        'image/pjpeg',
+        'image/png'
+    ];
+
+
+    public function __construct($path) {
+
+
+        if (!is_dir($path)) {
+            throw new \Exception("$path must be a valid, writable directory.");
+        }
+
+        $this->destination = $path;
+
+
+    }
+
+    public function upload() {
+
+        $uploaded = current($_FILES);
+
+
+        if ($this->checkFile($uploaded)) {
+
+            $this->moveFile($uploaded);
+        }
+    }
+
+
+    public function getMessages() {
+        return $this->messages;
+    }
+
+    public function checkFile() {
+      return true;
+    }
+
+    protected function moveFile($file) {
+
+        $success = move_uploaded_file($file['tmp_name'], $this->destination . $file['name']);
+
+
+        if ($success) {
+            $result = $file['name'] . ' was uploaded successfully';
+            $this->messages[] = $result;
+        } else {
+            $this->messages[] = 'Could not upload ' . $file['name'];
+        }
+    }
+}
+
+
+class book {
   public function __construct($title,$price)
   {
     $this->title=$title;
--- V17/VEF2A/V02/index.php
+++ V17/VEF2A/V02/index.php
@@ -1,5 +1,5 @@
 <?php
-include 'classes.php';
+include("classes.php");
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,11 +8,47 @@ include 'classes.php';
     <title>VEF2A V02 | ImageUpload</title>
 </head>
 <body>
+  <div id="imageShow">
+    <?php
+      $image = new image("Profile", 0,"siggi","Resources/Pictures/picture1.jpg");
+      $image->getImageSize();
+      $image->showImage();
+      $image->showImageSize();
+    ?>
+  </div>
   <div id ="imgupload">
-    <p>Hallo</p>
+    <p>image upload</p>
   </div>
   <div id ='image'>
-    <!--somthing something get url image -->
+    <?php
+    if (isset($_POST['upload'])) {
+      $destination = $_SERVER['DOCUMENT_ROOT'] . "\VEF2A\V02\Resources\Pictures/";
+      try {
+        $loader = new upload($destination);
+        $loader->upload();
+        $result = $loader->getMessages();
+      } catch (Exception $e) {
+        echo $e->getMessage();
+      }
+    }
+    if (isset($result)) {
+      echo '<ul>';
+      foreach ($result as $message) {
+        echo "<li>$message</li>";
+      }
+      echo '</ul>';
+      }
+    ?>
+
+    <form action="" method="post" enctype="multipart/form-data" id="uploadImage">
+        <p>
+            <label for="image">Upload image:</label>
+            <input type="file" name="image" id="image">
+        </p>
+        <p>
+            <input type="submit" name="upload" id="upload" value="Upload">
+        </p>
+    </form>
   </div>
 
 
