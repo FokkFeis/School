30b7065bdd1dfb041c79eca32bba410886ab7b09
Sigurður Baldvin Friðriksson <sigurdurbaldvi@gmail.com>
2017-02-02 00:00:15 +0000

    random, dont know excatly what was added'

--- V17/VEF2B/V03/codeacdemy.js
+++ V17/VEF2B/V03/codeacdemy.js
@@ -155,3 +155,425 @@
         // change bob's age to 50 here
         bob.setAge(50);
       //16.
+        // here we define our method using "this", before we even introduce bob
+        var setAge = function (newAge) {
+          this.age = newAge;
+        };
+        // now we make bob
+        var bob = new Object();
+        bob.age = 30;
+        bob.setAge = setAge;
+
+        // make susan here, and first give her an age of 25
+          var susan = new Object();
+          susan.age = 25;
+          susan.setAge = setAge;
+        // here, update Susan's age to 35 using the method
+          susan.setAge(35);
+      //17.
+        var rectangle = new Object();
+        rectangle.height = 3;
+        rectangle.width = 4;
+        // here is our method to set the height
+        rectangle.setHeight = function (newHeight) {
+        this.height = newHeight;
+        };
+        // help by finishing this method
+        rectangle.setWidth = function (newWidth) {
+          this.width = newWidth;
+        };
+
+        // here change the width to 8 and height to 6 using our new methods
+        rectangle.setHeight(6);
+        rectangle.setWidth(8);
+      //18.
+        var square = new Object();
+        square.sideLength = 6;
+        square.calcPerimeter = function() {
+        return this.sideLength * 4;
+        };
+        // help us define an area method here
+        square.calcArea = function() {
+          return this.sideLength * this.sideLength;
+        };
+
+        var p = square.calcPerimeter();
+        var a = square.calcArea();
+      //19.
+        // here we make bob using the Object constructor
+        var bob = new Object();
+        bob.name = "Bob Smith";
+        // add bob's age here and set it equal to 20
+        bob.age = 20;
+      //20.
+        function Person(name,age) {
+        this.name = name;
+        this.age = age;
+        }
+
+        // Let's make bob and susan again, using our constructor
+        var bob = new Person("Bob Smith", 30);
+        var susan = new Person("Susan Jordan", 25);
+        // help us make george, whose name is "George Washington" and age is 275
+        var george = new Person("George Washington",275);
+      //21.
+        function Cat(age, color) {
+        this.age = age;
+        this.color = color;
+        }
+
+        // make a Dog constructor here
+        function Dog(age, color, name) {
+          this.age = age;
+          this.color = color;
+          this.name = name;
+        }
+      //22.
+        function Person(name,age) {
+        this.name = name;
+        this.age = age;
+        this.species = "Homo Sapiens";
+        }
+
+        var sally = new Person("Sally Bowles", 39);
+        var holden =  new Person("Holden Caulfield", 16);
+        console.log("sally's species is " + sally.species + " and she is " + sally.age);
+        console.log("holden's species is " + holden.species + " and he is " + holden.age);
+      //23.
+        function Rectangle(height, width) {
+        this.height = height;
+        this.width = width;
+        this.calcArea = function() {
+            return this.height * this.width;
+        };
+        // put our perimeter function here!
+        this.calcPerimeter = function() {
+          return 2*(this.height + this.width);
+        };
+        }
+
+        var rex = new Rectangle(7,3);
+        var area = rex.calcArea();
+        var perimeter = rex.calcPerimeter();
+      //24.
+        function Rabbit(adjective) {
+          this.adjective = adjective;
+          this.describeMyself = function() {
+              console.log("I am a " + this.adjective + " rabbit");
+          };
+        }
+
+        // now we can easily make all of our rabbits
+        var rabbit1 = new Rabbit("fluffy");
+        var rabbit2 = new Rabbit("happy");
+        var rabbit3 = new Rabbit("sleepy");
+        rabbit1.describeMyself();
+        rabbit2.describeMyself();
+        rabbit3.descripeMyself();
+      //25.
+        // Our person constructor
+        function Person (name, age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        // Now we can make an array of people
+        var family = new Array();
+        family[0] = new Person("alice", 40);
+        family[1] = new Person("bob", 42);
+        family[2] = new Person("michelle", 8);
+        // add the last family member, "timmy", who is 6 years old
+        family[3] = new Person("timmy",6);
+      //26.
+        // Our Person constructor
+        function Person (name, age) {
+          this.name = name;
+          this.age = age;
+        }
+
+        // Now we can make an array of people
+        var family = new Array();
+        family[0] = new Person("alice", 40);
+        family[1] = new Person("bob", 42);
+        family[2] = new Person("michelle", 8);
+        family[3] = new Person("timmy",6);
+        // loop through our new array
+        for(var i = 0; i <family.length;i++){
+          console.log("My name is " + family[i].name);
+        };
+      //27.
+        // Our person constructor
+        function Person (name, age) {
+          this.name = name;
+          this.age = age;
+        }
+
+        // We can make a function which takes persons as arguments
+        // This one computes the difference in ages between two people
+        var ageDifference = function(person1, person2) {
+          return person1.age - person2.age;
+        }
+
+        var alice = new Person("Alice", 30);
+        var billy = new Person("Billy", 25);
+
+        // get the difference in age between alice and billy using our function
+        var diff = ageDifference(alice, billy);
+      //28.
+        // Our person constructor
+        function Person (name, age) {
+          this.name = name;
+          this.age = age;
+        }
+
+        // We can make a function which takes persons as arguments
+        // This one computes the difference in ages between two people
+        var ageDifference = function(person1, person2) {
+          return person1.age - person2.age;
+        };
+
+        // Make a new function, olderAge, to return the age of
+        // the older of two people
+        var olderAge = function(person1, person2){
+          if(person1.age>person2.age){
+            return person1.age;
+          }
+          else {
+            return person2.age;
+          }
+        };
+
+        // Let's bring back alice and billy to test our new function
+        var alice = new Person("Alice", 30);
+        var billy = new Person("Billy", 25);
+
+        console.log("The older person is " + olderAge(alice, billy));
+      //29.
+        var spencer = {
+        age: 22,
+        country: "United States"
+        };
+
+        // make spencer2 here with constructor notation
+        var spencer2 = new Object();
+        spencer2.age = 22;
+        spencer2.country = "United States";
+      //30.
+        var snoopy = new Object();
+        snoopy.species = "beagle";
+        snoopy.age = 10;
+
+        // save Snoopy's age and species into variables
+        // use dot notation for snoopy's species
+        var species = snoopy.species;
+
+        // use bracket notation for snoopy's age
+        var age = snoopy["age"];
+      //31.
+        // 3 lines required to make harry_potter
+        var harry_potter = new Object();
+        harry_potter.pages = 350;
+        harry_potter.author = "J.K. Rowling";
+
+        // A custom constructor for book
+        function Book (pages, author) {
+          this.pages = pages;
+          this.author = author;
+        }
+
+        // Use our new constructor to make the_hobbit in one line
+        var the_hobbit = new Book(320,"J.R.R. Tolkien");
+      //32.
+        function Circle (radius) {
+        this.radius = radius;
+        this.area = function () {
+            return Math.PI * this.radius * this.radius;
+
+        };
+        // define a perimeter method here
+        this.perimeter = function (radius) {
+            return 2 * Math.PI * this.radius;
+          };
+        };
+    //Building an address Book.
+      //1.
+        var bob = {
+          firstName: "Bob",
+          lastName: "Jones",
+          phoneNumber: "(650) 777-777",
+          email: "bob.jones@example.com"
+        };
+
+        console.log(bob.firstName);
+        console.log(bob.lastName);
+        console.log(bob.email);
+      //2.
+        var bob = {
+          firstName: "Bob",
+          lastName: "Jones",
+          phoneNumber: "(650) 777-7777",
+          email: "bob.jones@example.com"
+        };
+        var mary = {
+          firstName: "Mary",
+          lastName: "Jones",
+          phoneNumber: "(650) 888-8888",
+          email: "mary.johnsson@example.com"
+        };
+        var contacts = new Array();
+        contacts[0]= bob;
+        contacts[1]= mary;
+        console.log(contacts[1].phoneNumber);
+      //3.
+        var bob = {
+          firstName: "Bob",
+          lastName: "Jones",
+          phoneNumber: "(650) 777-7777",
+          email: "bob.jones@example.com"
+        };
+        var mary = {
+          firstName: "Mary",
+          lastName: "Jones",
+          phoneNumber: "(650) 888-8888",
+          email: "mary.johnsson@example.com"
+        };
+        var contacts = [bob, mary];
+
+        // printperson added here
+        function printPerson(person){
+          console.log(person.firstName + " " + person.lastName);
+        };
+        printPerson(contacts[0]);
+        printPerson(contacts[1]);
+      //4.
+        var bob = {
+          firstName: "Bob",
+          lastName: "Jones",
+          phoneNumber: "(650) 777-7777",
+          email: "bob.jones@example.com"
+        };
+        var mary = {
+          firstName: "Mary",
+          lastName: "Jones",
+          phoneNumber: "(650) 888-8888",
+          email: "mary.johnsson@example.com"
+        };
+        var contacts = [bob, mary];
+
+        // printperson added here
+        function printPerson(person){
+          console.log(person.firstName + " " + person.lastName);
+        };
+        function list(){
+          var contactsLength = contacts.length;
+          for(var i = 0; i < contactsLength; i++){
+            printPerson(contacts[i]);
+          };
+        };
+        list();
+      //5
+        var bob = {
+          firstName: "Bob",
+          lastName: "Jones",
+          phoneNumber: "(650) 777-7777",
+          email: "bob.jones@example.com"
+        };
+
+        var mary = {
+          firstName: "Mary",
+          lastName: "Johnson",
+          phoneNumber: "(650) 888-8888",
+          email: "mary.johnson@example.com"
+        };
+
+        var contacts = [bob, mary];
+        var contactsLength = contacts.length;
+        function printPerson(person) {
+          console.log(person.firstName + " " + person.lastName);
+        }
+
+        function list() {
+        var contactsLength = contacts.length;
+        for (var i = 0; i < contactsLength; i++) {
+          printPerson(contacts[i]);
+        };
+        }
+
+        /*Create a search function
+        then call it passing "Jones"*/
+        function search(lastName){
+
+          for(var i = 0; i < contactsLength; i++){
+            if(contacts[i].lastName == lastName){
+                  printPerson(contacts[i]);
+              };
+          };
+        }
+        search("Jones");
+      //6.
+        var bob = {
+          firstName: "Bob",
+          lastName: "Jones",
+          phoneNumber: "(650) 777-7777",
+          email: "bob.jones@example.com"
+        };
+
+        var mary = {
+          firstName: "Mary",
+          lastName: "Johnson",
+          phoneNumber: "(650) 888-8888",
+          email: "mary.johnson@example.com"
+        };
+
+        var contacts = [bob, mary];
+        var contactsLength = contacts.length;
+        function printPerson(person) {
+          console.log(person.firstName + " " + person.lastName);
+        }
+
+        function list() {
+        var contactsLength = contacts.length;
+        for (var i = 0; i < contactsLength; i++) {
+          printPerson(contacts[i]);
+        };
+        }
+
+        /*Create a search function
+        then call it passing "Jones"*/
+        function search(lastName){
+
+          for(var i = 0; i < contactsLength; i++){
+            if(contacts[i].lastName == lastName){
+                  printPerson(contacts[i]);
+              };
+          };
+        }
+
+        function add(firstName, lastName, email, phoneNumber){
+          var person = {
+              firstName: firstName,
+              lastName: lastName,
+              email: email,
+              phoneNumber: phoneNumber
+          };
+          contacts[contactsLength] = person;
+        }
+        add("Sigurdur", "Baldvin","siggi@example.com","581-2345");
+        list();
+    //Introduction to Objects II.
+      //1.
+        var james = {
+          // add properties to this object!
+          job: "programmer",
+          married: false
+
+        };
+
+        function Person(job, married) {
+          this.job = job;
+          this.married = married;
+        }
+
+        // create a "gabby" object using the Person constructor!
+        var gabby = new Person("student", true);
+      //2.
