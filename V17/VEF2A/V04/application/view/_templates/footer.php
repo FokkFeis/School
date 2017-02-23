c5940f8d3eae6bdb35c11b5e4c4a0f29ac77067d
Sigurður Baldvin Friðriksson <sigurdurbaldvi@gmail.com>
2017-02-23 22:23:55 +0000

    added V04 in VEF2A

---
+++ V17/VEF2A/V04/application/view/_templates/footer.php
@@ -0,0 +1,20 @@
+<?php if (!$this) { exit(header('HTTP/1.0 403 Forbidden')); }
+$footer = '<footer>
+  <p>Sigurður Baldvin Friðrikson &#169;</p>
+</footer>';
+echo $footer;
+?>
+
+    <!-- jQuery, loaded in the recommended protocol-less way -->
+    <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <!-- define the project's URL (to make AJAX calls possible, even when using this in sub-folders etc) -->
+    <script>
+        var url = "<?php echo URL; ?>";
+    </script>
+
+    <!-- our JavaScript -->
+    <script src="<?php echo URL; ?>js/application.js"></script>
+</body>
+</html>
