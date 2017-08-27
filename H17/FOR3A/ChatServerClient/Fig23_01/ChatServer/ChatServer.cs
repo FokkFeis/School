9eba7bf258347b421f5304fbcde23e00dbdc617e
Sigurður Baldvin Friðriksson <sigurdurbaldvi@gmail.comn>
2017-08-27 13:03:21 +0000

    Folder Structure

---
+++ H17/FOR3A/ChatServerClient/Fig23_01/ChatServer/ChatServer.cs
@@ -0,0 +1,180 @@
+// Fig. 23.1: ChatServer.cs
+// Set up a server that will receive a connection from a client, send a
+// string to the client, chat with the client and close the connection.
+using System;
+using System.Windows.Forms;
+using System.Threading;  
+using System.Net;        
+using System.Net.Sockets;
+using System.IO;         
+
+public partial class ChatServerForm : Form
+{
+   public ChatServerForm()
+   {
+      InitializeComponent();
+   } // end constructor
+
+   private Socket connection; // Socket for accepting a connection      
+   private Thread readThread; // Thread for processing incoming messages
+   private NetworkStream socketStream; // network data stream           
+   private BinaryWriter writer; // facilitates writing to the stream    
+   private BinaryReader reader; // facilitates reading from the stream  
+
+   // initialize thread for reading
+   private void ChatServerForm_Load( object sender, EventArgs e )
+   {
+      readThread = new Thread( new ThreadStart( RunServer ) );
+      readThread.Start();                                     
+   } // end method CharServerForm_Load
+
+   // close all threads associated with this application
+   private void ChatServerForm_FormClosing( object sender, 
+      FormClosingEventArgs e )
+   {         
+      System.Environment.Exit( System.Environment.ExitCode );
+   } // end method CharServerForm_FormClosing
+
+   // delegate that allows method DisplayMessage to be called
+   // in the thread that creates and maintains the GUI       
+   private delegate void DisplayDelegate( string message );  
+
+   // method DisplayMessage sets displayTextBox's Text property
+   // in a thread-safe manner
+   private void DisplayMessage( string message )
+   {
+      // if modifying displayTextBox is not thread safe
+      if ( displayTextBox.InvokeRequired )
+      {
+         // use inherited method Invoke to execute DisplayMessage
+         // via a delegate                                       
+         Invoke( new DisplayDelegate( DisplayMessage ),          
+            new object[] { message } );                          
+      } // end if
+      else // OK to modify displayTextBox in current thread
+         displayTextBox.Text += message;
+   } // end method DisplayMessage
+
+   // delegate that allows method DisableInput to be called 
+   // in the thread that creates and maintains the GUI
+   private delegate void DisableInputDelegate( bool value );
+
+   // method DisableInput sets inputTextBox's ReadOnly property
+   // in a thread-safe manner
+   private void DisableInput( bool value )
+   {
+      // if modifying inputTextBox is not thread safe
+      if ( inputTextBox.InvokeRequired )
+      {
+         // use inherited method Invoke to execute DisableInput
+         // via a delegate                                     
+         Invoke( new DisableInputDelegate( DisableInput ),     
+            new object[] { value } );                          
+      } // end if
+      else // OK to modify inputTextBox in current thread
+         inputTextBox.ReadOnly = value;
+   } // end method DisableInput
+
+   // send the text typed at the server to the client
+   private void inputTextBox_KeyDown( object sender, KeyEventArgs e )
+   {
+      // send the text to the client
+      try
+      {         
+         if ( e.KeyCode == Keys.Enter && inputTextBox.ReadOnly == false )
+         {
+            writer.Write( "SERVER>>> " + inputTextBox.Text );
+            displayTextBox.Text += "\r\nSERVER>>> " + inputTextBox.Text;
+
+            // if the user at the server signaled termination
+            // sever the connection to the client
+            if ( inputTextBox.Text == "TERMINATE" )
+               connection.Close();
+
+            inputTextBox.Clear(); // clear the user’s input
+         } // end if
+      } // end try
+      catch ( SocketException )
+      {
+         displayTextBox.Text += "\nError writing object";
+      } // end catch
+   } // end method inputTextBox_KeyDown
+
+   // allows a client to connect; displays text the client sends
+   public void RunServer()
+   {
+      TcpListener listener;
+      int counter = 1;
+
+      // wait for a client connection and display the text
+      // that the client sends
+      try
+      {
+         // Step 1: create TcpListener                    
+         IPAddress local = IPAddress.Parse( "127.0.0.1" );
+         listener = new TcpListener( local, 50000 );       
+
+         // Step 2: TcpListener waits for connection request
+         listener.Start();                                  
+
+         // Step 3: establish connection upon client request
+         while ( true )
+         {
+            DisplayMessage( "Waiting for connection\r\n" );
+
+            // accept an incoming connection     
+            connection = listener.AcceptSocket();
+
+            // create NetworkStream object associated with socket
+            socketStream = new NetworkStream( connection );      
+
+            // create objects for transferring data across stream
+            writer = new BinaryWriter( socketStream );           
+            reader = new BinaryReader( socketStream );           
+
+            DisplayMessage( "Connection " + counter + " received.\r\n" );
+
+            // inform client that connection was successfull  
+            writer.Write( "SERVER>>> Connection successful" );
+
+            DisableInput( false ); // enable inputTextBox
+
+            string theReply = "";
+
+            // Step 4: read string data sent from client
+            do
+            {
+               try
+               {   
+                  // read the string sent to the server
+                  theReply = reader.ReadString();
+
+                  // display the message
+                  DisplayMessage( "\r\n" + theReply );
+               } // end try
+               catch ( Exception )
+               {
+                  // handle exception if error reading data
+                  break;
+               } // end catch
+            } while ( theReply != "CLIENT>>> TERMINATE"  &&
+               connection.Connected );
+
+            DisplayMessage( "\r\nUser terminated connection\r\n" );
+
+            // Step 5: close connection  
+            writer.Close();              
+            reader.Close();              
+            socketStream.Close();        
+            connection.Close();          
+
+            DisableInput( true ); // disable InputTextBox
+            counter++;
+         } // end while
+      } // end try
+      catch ( Exception error )
+      {
+         MessageBox.Show( error.ToString() );
+      } // end catch
+   } // end method RunServer
+} // end class ChatServerForm
---
+++ H17/FOR3A/V01/TickingClock/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
+
+namespace TickingClock
+{
+    class WorldTime : Form
+    {
+        PictureBox picture;
+		PictureBox digiPicture;
+        Thread clockThread;
+
+        bool ended = false;
+
+        protected override void Dispose(bool disposing)
+        {
+            ended = true;
+            Console.WriteLine("Wait while we shut down...");
+            clockThread.Join();
+            base.Dispose(disposing);
+            Application.Exit();
+        }
+
+        WorldTime()
+        {
+            Text = "Clock";
+           
+            // Set up a picture box 
+            picture = new PictureBox();
+            picture.Size = new Size(150, 150);
+            // add the picture to the form
+            Controls.Add(picture);
+
+			digiPicture = new PictureBox ();
+			digiPicture.Size = new Size (150, 150);
+
+			Controls.Add (digiPicture);
+            
+            // add the event 
+            picture.Paint += new PaintEventHandler(new Clock().ClockPaint);
+            // create and start a thread to control the clock
+            clockThread = new Thread(new ThreadStart(TickClock));
+            clockThread.Start();
+        }
+
+
+        void TickClock()
+        {
+            while (!ended)
+            {
+                picture.Invalidate();
+                Thread.Sleep(1000);
+            }
+        }
+	
+        public class Clock
+        {
+            int seconds = 0;
+            SolidBrush bluebrush, whitebrush;
+            Pen blackpen;
+            int start = 100;
+
+            public Clock()
+            {
+                blackpen = new Pen(Color.Black);
+                bluebrush = new SolidBrush(Color.Blue);
+                whitebrush = new SolidBrush(Color.LightGray);
+            }
+
+            // The handler redraws the whole clock
+            public void ClockPaint(object source, PaintEventArgs e)
+            {
+                Graphics g = e.Graphics;
+                g.DrawEllipse(blackpen, start / 2, start / 2, 100, 100);
+                g.FillEllipse(whitebrush, start / 2, start / 2, 100, 100);
+                g.FillEllipse(bluebrush, start, start, 5, 5);
+                Hands(g);
+                seconds++;
+            }
+
+            void Hands(Graphics g)
+            {
+                double hourAngle = 2 * Math.PI * (seconds - 3 * 60 * 60) / (12 * 60 * 60);
+                double minuteAngle = 2 * Math.PI * (seconds - 15 * 60) / (60 * 60);
+                double secondAngle = 2 * Math.PI * (seconds - 15) / 60;
+                g.DrawLine(blackpen, start, start,
+                    start + (int)(30 * Math.Cos(hourAngle)),
+                    start + (int)(30 * Math.Sin(hourAngle)) + 5);
+                g.DrawLine(blackpen, start, start,
+                    start + (int)(40 * Math.Cos(minuteAngle)),
+                    start + (int)(40 * Math.Sin(minuteAngle)));
+                g.DrawLine(blackpen, start, start,
+                    start + (int)(45 * Math.Cos(secondAngle)),
+                    start + (int)(45 * Math.Sin(secondAngle)));
+            }
+        }
+
+        static void Main()
+        {
+            Application.Run(new WorldTime());
+        }
+    }
+}
